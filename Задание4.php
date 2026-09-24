90c6edaabccf4cc8c2302d6ad36c078bae4f7f2a
Kr4fol <aleksander3sit@gmail.com>
2026-09-24 09:44:19 +0300

    Create Задание4.php

---
+++ Задание4.php
@@ -0,0 +1,27 @@
+<?php
+$day = 1;
+switch ($day) {
+    case 1:
+        echo 'понедельник';
+        break;
+    case 2:
+        echo 'вторник';
+        break;
+    case 3:
+        echo 'среда';
+        break;
+    case 4:
+        echo 'четверг';
+        break;
+    case 5:
+        echo 'пятница';
+        break;
+    case 6:
+        echo 'суббота';
+        break;
+    case 7:
+        echo 'воскресенье';
+        break;
+    default:
+        echo 'ошибка';
+}
